0eba5aeb3a423af9dead050470ab536f4b745240
E-lysian <itsmeelysian@outlook.com>
2023-05-29 03:16:38 -0700

    Spawn object offset fixed

--- Scape05/Networking/Packets/Outgoing/SpawnGroundItemPacketCommand.cs
+++ Scape05/Networking/Packets/Outgoing/SpawnGroundItemPacketCommand.cs
@@ -6,23 +6,25 @@ public class SpawnGroundItemPacketCommand : IPacketCommand
 {
     private readonly int itemId;
     private readonly int amount;
+    private readonly Location _spawnLocation;
 
     private SpawnGroundItemPacketCommand(int itemId, int amount, Location spawnLocation)
     {
         this.itemId = itemId;
         this.amount = amount;
+        _spawnLocation = spawnLocation;
     }
 
     public void Execute(Player player)
     {
         player.Writer.CreateFrame(ServerOpCodes.PLAYER_LOCATION);
-        player.Writer.WriteByteC(player.Location.Y - 8 * player.BuildArea.OffsetChunkY);
-        player.Writer.WriteByteC(player.Location.X - 8 * player.BuildArea.OffsetChunkX);
+        player.Writer.WriteByteC(((player.Location.Y >> 3) << 3) - player.BuildArea.OffsetChunkY * 8);
+        player.Writer.WriteByteC(((player.Location.X >> 3) << 3) - player.BuildArea.OffsetChunkX * 8);
         
         player.Writer.CreateFrame(ServerOpCodes.FLOORITEM_ADD);
         player.Writer.WriteWordBigEndianA(1042);
         player.Writer.WriteWord(1);
-        player.Writer.WriteByte(0);
+        player.Writer.WriteByte(((_spawnLocation.X & 0x7) << 4) | (_spawnLocation.Y & 0x7));
     }
 
     public static SpawnGroundItemPacketCommand Create(int itemId, int amount, Location spawnLocation)
